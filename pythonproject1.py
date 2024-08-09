a415876bd0fbda81c08849e9b2aec70539be2030
Jacinta Haynes <101839863+J-H731@users.noreply.github.com>
2024-08-09 19:36:18 -0400

    Making file names more understandable

--- pythonproject1.py
+++
@@ -1,4 +0,0 @@
-print("Hello World")
-name = str(input("Please enter your name: "))
-#"str" lets the programme know that it is a string and not a number.
-#don't need to specify "str" for letter inputs as that is done by the programme automatically.
