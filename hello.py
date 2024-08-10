470224752e03238cbd9e434285c776e3259c6fb0
Jacinta Haynes <101839863+J-H731@users.noreply.github.com>
2024-08-09 20:23:25 -0400

    Learning more basics of python in hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-print("Hello, world!")
-name = str(input("Please enter your name: "))
-# "str" lets the programme know that it is a string and not a number.
-# don't need to specify "str" for letter inputs as that is done by the programme automatically.
-print("Hello, " + name)
+print("Hello, world! \n What is your name?")
+name = str(input("Enter your name: ")).strip().title()
+# "str" lets the programme know that it is a string and not a number, but it is done automatically
+# removes whitespace and capitalize name
+
+first, last = name.split()
+
+print(f"Hello, {name}")
